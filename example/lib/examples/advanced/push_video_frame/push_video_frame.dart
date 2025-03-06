45da344ef0d2616c71bf59d3f879d92f4cccac7a
peilinok <peilinok@users.noreply.github.com>
2025-03-06 08:01:18 +0000

    [AUTO]change package from agora to shengwang

--- example/lib/examples/advanced/push_video_frame/push_video_frame.dart
+++ example/lib/examples/advanced/push_video_frame/push_video_frame.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 
-import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:shengwang_rtc_engine/agora_rtc_engine.dart';
 import 'package:agora_rtc_engine_example/config/agora.config.dart' as config;
 import 'package:agora_rtc_engine_example/components/example_actions_widget.dart';
 import 'package:agora_rtc_engine_example/components/log_sink.dart';
